a453be0e575fc3f14757303e74a3367247c18bc1
Miswanting <453542772@qq.com>
2019-07-27 19:40:24 +0800

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize,Deserialize };
 use std::fs;
 use std::path::Path;
 use std::time;
-use toml::Value;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    ignore_file: Vec<String>
+}
+
 #[derive(Serialize, Deserialize)]
 struct Database {
     time: u64,
     data: Vec<FileInfo>,
 }
+
 #[derive(Serialize, Deserialize)]
 struct FileInfo {
     path: String,
     time: u64,
 }
+
 const DEFAULT_MAJOR: u8 = 0;
 const DEFAULT_MINOR: u8 = 1;
 const DEFAULT_PATCH: u8 = 0;
+const CONFIG_FILE_NAME:&str = "config.toml";
+const DATA_FILE_NAME:&str = "db.json";
+
 // const SUPPORT_EXT:Vec<&str>= vec!["doc"];
 fn main() {
+    let mut config = Config {
+        ignore_file: vec![]
+    };
     let mut db = Database {
         time: time::SystemTime::now()
             .duration_since(time::UNIX_EPOCH)
@@ -25,19 +38,22 @@ fn main() {
             .as_secs(),
         data: Vec::new(),
     };
-    let db_path = Path::new("db.toml");
+    let db_path = Path::new(DATA_FILE_NAME);
+    if db_path.exists() {// DEBUG
+        fs::remove_file(DATA_FILE_NAME).unwrap();
+    }
     if db_path.exists() {
         // 比对数据库
     } else {
         // 生成数据库
         let path = Path::new(".");
         init(path, &mut db);
-        let toml = toml::to_string(&db).unwrap();
-        fs::write("db.toml", toml).unwrap();
+        let json = serde_json::to_string(&db).unwrap();
+        fs::write(DATA_FILE_NAME, json).unwrap();
     };
 
-    let value = "foo = 'bar'".parse::<Value>().unwrap();
-    println!("{:?}", value["foo"].as_str())
+    // let value = "foo = 'bar'".parse::<Value>().unwrap();
+    // println!("{:?}", value["foo"].as_str())
     // let path = Path::new(".");
     // search_path(path);
 }
