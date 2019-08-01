36ebf3117998450b954cca2e69a92a4ed6841dbb
Miswanting <453542772@qq.com>
2019-08-01 16:57:32 +0800

    GOGOGO

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use serde::{Serialize,Deserialize };
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
 use std::time;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Config {
-    ignore_file: Vec<String>
+    ignore_file: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Database {
     time: u64,
     data: Vec<FileInfo>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct FileInfo {
     path: String,
     time: u64,
@@ -23,13 +23,12 @@ struct FileInfo {
 const DEFAULT_MAJOR: u8 = 0;
 const DEFAULT_MINOR: u8 = 1;
 const DEFAULT_PATCH: u8 = 0;
-const CONFIG_FILE_NAME:&str = "config.toml";
-const DATA_FILE_NAME:&str = "db.json";
+const CONFIG_FILE_NAME: &str = "config.yaml";
+const DATABASE_FILE_NAME: &str = "database.json";
 
-// const SUPPORT_EXT:Vec<&str>= vec!["doc"];
 fn main() {
-    let mut config = Config {
-        ignore_file: vec![]
+    let mut cf = Config {
+        ignore_file: "123".to_string(),
     };
     let mut db = Database {
         time: time::SystemTime::now()
@@ -38,32 +37,42 @@ fn main() {
             .as_secs(),
         data: Vec::new(),
     };
-    let db_path = Path::new(DATA_FILE_NAME);
-    if db_path.exists() {// DEBUG
-        fs::remove_file(DATA_FILE_NAME).unwrap();
+    let cf_path = Path::new(CONFIG_FILE_NAME);
+    let db_path = Path::new(DATABASE_FILE_NAME);
+    if cf_path.exists() {
+        // DEBUG
+        fs::remove_file(CONFIG_FILE_NAME).unwrap();
     }
+    if db_path.exists() {
+        // DEBUG
+        fs::remove_file(DATABASE_FILE_NAME).unwrap();
+    }
+    if cf_path.exists() {
+        // 读取配置
+        let f = fs::read_to_string(CONFIG_FILE_NAME).unwrap();
+        cf = serde_yaml::from_str(&f).unwrap();
+    } else {
+        // 生成默认配置
+        let yaml = serde_yaml::to_string(&cf).unwrap();
+        fs::write(CONFIG_FILE_NAME, yaml).unwrap();
+    };
     if db_path.exists() {
         // 比对数据库
     } else {
         // 生成数据库
         let path = Path::new(".");
-        init(path, &mut db);
+        init_scan(path, &mut db);
         let json = serde_json::to_string(&db).unwrap();
-        fs::write(DATA_FILE_NAME, json).unwrap();
+        fs::write(DATABASE_FILE_NAME, json).unwrap();
     };
-
-    // let value = "foo = 'bar'".parse::<Value>().unwrap();
-    // println!("{:?}", value["foo"].as_str())
-    // let path = Path::new(".");
-    // search_path(path);
 }
-fn init(root: &Path, db: &mut Database) {
+fn init_scan(root: &Path, db: &mut Database) {
     let root_path = Path::new(root);
     for each in fs::read_dir(root_path).unwrap() {
         let dir = each.unwrap();
         let pb = dir.path();
         if pb.is_dir() {
-            init(&pb, db)
+            init_scan(&pb, db)
         } else {
             let metadata = fs::metadata(&pb);
             db.data.push(FileInfo {
@@ -79,6 +88,28 @@ fn init(root: &Path, db: &mut Database) {
         }
     }
 }
+// fn init(root: &Path, db: &mut Database) {
+//     let root_path = Path::new(root);
+//     for each in fs::read_dir(root_path).unwrap() {
+//         let dir = each.unwrap();
+//         let pb = dir.path();
+//         if pb.is_dir() {
+//             init(&pb, db)
+//         } else {
+//             let metadata = fs::metadata(&pb);
+//             db.data.push(FileInfo {
+//                 path: pb.to_str().unwrap().to_string(),
+//                 time: metadata
+//                     .unwrap()
+//                     .modified()
+//                     .unwrap()
+//                     .duration_since(time::UNIX_EPOCH)
+//                     .unwrap()
+//                     .as_secs(),
+//             });
+//         }
+//     }
+// }
 fn search_path(path: &Path) {
     for each in fs::read_dir(path).unwrap() {
         let dir = each.unwrap();
@@ -90,3 +121,5 @@ fn search_path(path: &Path) {
         }
     }
 }
+fn file2data() {}
+fn data2file() {}
